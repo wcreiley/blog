d1c58ac9771dd84cbe26ab4bcd8a93f28fa09d84
Wes Reiley <wesley_reiley@yahoo.com>
2017-01-02 15:35:36 -0600

    Fixed millisecond/second typo

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,11 +1,11 @@
 class MetricsController < ApplicationController
   def metrics
-    @average_db_lookup_in_ms=0
+    @average_db_lookup_in_s=0
     
     startTime = Time.now
     Article.all
     endTime = Time.now
-    @average_db_lookup_in_ms = endTime - startTime
+    @average_db_lookup_in_s = endTime - startTime
   end
 
 end
